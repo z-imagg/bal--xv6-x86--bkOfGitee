52c4f9612519e40cdc62a25b571eb8f7a4090242
prgrmz01@163.com <prgrmz01@163.com>
2023-11-10 15:23:20 +0800

    02.c正确一次：原来c中嵌入汇编带变量时原来的%要改为%%

--- assebly_study/02.c
+++ assebly_study/02.c
@@ -1,7 +1,6 @@
 void f(){
 	int pa;
-    __asm__  __volatile__ ( 
- 
+    __asm__  __volatile__ (  "movl %%eax, 0x7b00 \n\t"  
       "movl %0, %%eax \n\t"    
       "nop \n\t"     
       :  
--- assebly_study/02.sh
+++ assebly_study/02.sh
@@ -1,1 +1,3 @@
 gcc -fno-pic -static -fno-builtin -fno-strict-aliasing -O2 -Wall -MD -ggdb -m32 -Werror -fno-omit-frame-pointer -fno-stack-protector -fno-pic -O -nostdinc -I. -c 02.c
+objdump -S 02.o > 02.o-S.txt
+cat 02.o-S.txt
