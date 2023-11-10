cdec4a1b99bb3d98977ac61e73b893062f96644c
prgrmz01@163.com <prgrmz01@163.com>
2023-11-10 15:37:09 +0800

    gas inline汇编 （即 AT&T inline汇编） 当有变量带入时, %寄存器 改为 %%寄存器

--- bootmain.c
+++ bootmain.c
@@ -39,15 +39,15 @@ bootmain(void)
     __asm__  __volatile__ (
       "movw $0XBB77, 0x7c00-16 \n\t"  //写内存标记 供gdb条件断点使用
 
-      "movl %eax, 0x7c00-48 \n\t"   //备份eax
+      "movl %%eax, 0x7c00-48 \n\t"   //备份eax
 
       "movl %0, %%eax \n\t"   
-      "movw %eax, 0x7c00-32 \n\t"   //记录变量pa值
+      "movl %%eax, 0x7c00-32 \n\t"   //记录变量pa值
 
-      "movl 0x7c00-48,%eax  \n\t"   //恢复eax
+      "movl 0x7c00-48,%%eax  \n\t"   //恢复eax
       "nop \n\t"    
       : 
-      : "m"(pa)
+      : "m"(pa) //gas inline汇编 （即 AT&T inline汇编） 当有变量带入时, %寄存器 改为 %%寄存器
        );// 正常查看pa值 
 	  /* gdb单步发现此两条指令如下:
 		0x7d81  test   %edx,-0x1c(%ebp)                                                                                                                 │
