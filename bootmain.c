0261305cf00a2341226e1542fa5a778bd9f08635
prgrmz01@163.com <prgrmz01@163.com>
2023-11-11 00:15:42 +0800

    dev3

--- bootmain.c
+++ bootmain.c
@@ -37,12 +37,12 @@ bootmain(void)
   for(; ph < eph; ph++){
     pa = (uchar*)ph->paddr;
     __asm__  __volatile__ (
-      "movw $0XBB77, 0x7c00-16 \n\t"  //写内存标记 供gdb条件断点使用
 
       "movl %%eax, 0x7c00-48 \n\t"   //备份eax
 
       "movl %0, %%eax \n\t"   
       "movl %%eax, 0x7c00-32 \n\t"   //记录变量pa值
+      "movl 0x7c00-32,%%eax \n\t"   //无用读一次, 为了gdb awatch能奏效
 
       "movl 0x7c00-48,%%eax  \n\t"   //恢复eax
       "nop \n\t"    
