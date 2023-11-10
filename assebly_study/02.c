7501c89c48acbd7b4a76897363fe983452b1365a
prgrmz01@163.com <prgrmz01@163.com>
2023-11-10 15:33:47 +0800

    02.c: 正确：全：手工添加注释

--- assebly_study/02.c
+++ assebly_study/02.c
@@ -1,10 +1,14 @@
 void f(){
 	int pa;
-    __asm__  __volatile__ (  "movl %%eax, 0x7b00 \n\t"  
-      "movl %0, %%eax \n\t"    
-      "nop \n\t"     
-      :  
-      : "m"(pa) 
+    __asm__  __volatile__ (  
+	"movw $0XBB77, 0x7c00-16 \n\t"  //写内存标记 供gdb条件断点使用 
+	"movl %%eax, 0x7c00-48 \n\t"   //备份eax 
+	"movl %0, %%eax \n\t"    
+	"movl %%eax, 0x7c00-32 \n\t"   //记录变量pa值 
+	"movl 0x7c00-48,%%eax  \n\t"   //恢复eax 
+	"nop \n\t"     
+	:  
+	: "m"(pa) 
        );
 }
 
