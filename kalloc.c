42b347a977d2f47e829a806da8a35f6bc198aa82
prgrmz07 <prgrmz07@asus.zzz>
2023-11-13 17:49:05 +0800

    inline汇编写到同一行，正常编译运行

--- kalloc.c
+++ kalloc.c
@@ -31,14 +31,7 @@ struct {
 void
 kinit1(void *vstart, void *vend)
 {
-      __asm__  __volatile__ ( 
-      "jmp origin_begin \n\t"      //跳到函数原始开头第一条语句
-      "or $0xFFFFFFFF,%%edi \n\t"   //标记（以尽可能降低与正常指令相同的可能)
-      "or $0x12345678,%%edi \n\t"   //函数id为0x12345678
-      "origin_begin: \n\t"         //函数原始开头第一条语句的标号
-      :  
-      :  
-       );
+      __asm__  __volatile__ ( "jmp origin_begin \n\t"      /*跳到函数原始开头第一条语句*/   "or $0xFFFFFFFF,%%edi \n\t"   /*标记（以尽可能降低与正常指令相同的可能)*/       "or $0x12345678,%%edi \n\t"   /*函数id为0x12345678*/  "origin_begin: \n\t"         /*函数原始开头第一条语句的标号*/  :    :  );
   initlock(&kmem.lock, "kmem");
   kmem.use_lock = 0;
   freerange(vstart, vend);
